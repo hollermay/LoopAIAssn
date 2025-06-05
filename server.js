6286fa2dc1f4507a9fb232965d4fe159ee79930a
hollermay <udayanmoudgil@gmail.com>
2025-06-05 11:14:20 +0530

    added port configuration for heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const app = require('./app');
 const { processQueue } = require('./jobs/jobs');
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
   processQueue();
 });
-
 app.get('/', (req, res) => {
   res.send('Hey LoopAI, Udayan here :), the backend is up and GodSpeed!');
 })
