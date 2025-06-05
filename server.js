bbf8127e08b784fa9a142e800191834d06423bf0
hollermay <udayanmoudgil@gmail.com>
2025-06-05 11:15:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const app = require('./app');
 const { processQueue } = require('./jobs/jobs');
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   processQueue();
 });
+
 app.get('/', (req, res) => {
   res.send('Hey LoopAI, Udayan here :), the backend is up and GodSpeed!');
 })
